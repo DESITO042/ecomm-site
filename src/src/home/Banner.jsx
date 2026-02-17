805b0772951733f1cdc423a76c7257ee46b60b3c
DESITO042 <initialcoin333@gmail.com>
2026-02-16 23:01:50 -0800

    Fix import path for SelectedCategory component

--- src/src/home/Banner.jsx
+++ src/src/home/Banner.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import productData from "../products.json"
 import { Link } from 'react-router-dom'
-import SelectedCategory from '../components/selectedCategory'
+import SelectedCategory from './components/selectedCategory'
 
 const title = (
     <h2>Search Your One From <span>Thousands</span> Of Product</h2>
